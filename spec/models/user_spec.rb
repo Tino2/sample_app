ec990dec5be3128df341ced08fde4caab8414e49
Florentino Alvidrez <tinoalvidrez@gmail.com>
2014-05-26 12:04:42 -0600

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,15 +8,19 @@ describe User do
   end
 
   subject { @user }
-
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
   it { should respond_to(:authenticate) }
+
+
+
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -104,4 +108,10 @@ describe "with a password that's too short" do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
 end
+
